b59eac1e79028599a9f993a65554cb353f344bc8
SpyMonarch <muhammadaji1614@gmail.com>
2023-07-26 00:22:32 +0700

    create hero section

--- app/views/index.php
+++ app/views/index.php
@@ -1,15 +1,24 @@
 <section id="home" class="">
     <div class="container-sm ">
         <!-- GANTI HOME MULAI DARI SINI-->
-        <div class="bgAnimation" id="bgAnimation">
+        <!-- <div class="bgAnimation" id="bgAnimation">
             <div class="backgroundAmim">
-
             </div>
-        </div>
+        </div> -->
 
-        <div id="root">
-
-        </div>
+        <section class="hero text-center" id="">
+            <div class="container">
+                <!-- image  -->
+                <img src="/public/img/fav.png" width="112" alt="hero-img">
+                <!-- Text -->
+                <div class="text">
+                    <h1 class="display-4">Scriptshare</h1>
+                    <p>Bersama kami, bagikan tulisan Anda disini</p>
+                </div>
+                <a href="" class="btn btn-primary rounded-pill">Join with Us</a>
+                <a href="" class="btn btn-outline-light ms-3 rounded-pill">Learn more</a>
+            </div>
+        </section>
 
         <!-- <div class="card mx-auto bg-hero text-light" style="max-width: 740px;">
             <div class="row g-0">
@@ -55,13 +64,13 @@
 </section>
 
 <script>
-const bgAnimation = document.getElementById('bgAnimation');
+    const bgAnimation = document.getElementById('bgAnimation');
 
-const numberOfColorBoxes = 400;
+    const numberOfColorBoxes = 400;
 
-for (let i = 0; i < numberOfColorBoxes; i++) {
-    const colorBox = document.createElement('div');
-    colorBox.classList.add('colorBox');
-    bgAnimation.append(colorBox)
-}
+    for (let i = 0; i < numberOfColorBoxes; i++) {
+        const colorBox = document.createElement('div');
+        colorBox.classList.add('colorBox');
+        bgAnimation.append(colorBox)
+    }
 </script>
