a68c5390b3be996468ec0a5df9ccf943f4a8e956
Hyugenk <kojroewhyuga@gmail.com>
2023-07-26 06:56:55 +0700

    cekk

--- app/views/index.php
+++ app/views/index.php
@@ -36,7 +36,7 @@
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">INI NANTI HOME</h5>
+                        <h5 class="card-title">INI NANTI HOME, OKe bro</h5>
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
                             additional content. This content is a little bit longer.</p>
                         <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
