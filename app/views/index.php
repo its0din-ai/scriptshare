60830c682b84167f24b0325aa34a7f327613a24a
SpyMonarch <muhammadaji1614@gmail.com>
2023-07-25 15:54:44 +0700

    ganti profile

--- app/views/index.php
+++ app/views/index.php
@@ -6,21 +6,9 @@
 
             </div>
         </div>
+        <div class="">
+            <h1>Selamat Datang di </h1>
 
-        <div class="card mx-auto" style="max-width: 540px;">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="./public/img/brand-lg.png" class="img-fluid rounded-start" alt="...">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">INI NANTI RUMAH RUBAH</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-            </div>
         </div>
 
         <!-- SAMPAI SINI-->
