54d37b8aa0bb40c0a324bf7444137c79eab12b44
SpyMonarch <muhammadaji1614@gmail.com>
2023-07-26 00:40:23 +0700

    animasi & hero section

--- app/views/index.php
+++ app/views/index.php
@@ -1,10 +1,10 @@
 <section id="home" class="">
     <div class="container-sm ">
         <!-- GANTI HOME MULAI DARI SINI-->
-        <!-- <div class="bgAnimation" id="bgAnimation">
+        <div class="bgAnimation" id="bgAnimation">
             <div class="backgroundAmim">
             </div>
-        </div> -->
+        </div>
 
         <section class="hero text-center" id="">
             <div class="container">
@@ -15,25 +15,10 @@
                     <h1 class="display-4">Scriptshare</h1>
                     <p>Bersama kami, bagikan tulisan Anda disini</p>
                 </div>
-                <a href="" class="btn btn-primary rounded-pill">Join with Us</a>
+                <a href="" class="btn btn-warning rounded-pill ">Join with Us</a>
                 <a href="" class="btn btn-outline-light ms-3 rounded-pill">Learn more</a>
             </div>
         </section>
-
-        <!-- <div class="card mx-auto bg-hero text-light" style="max-width: 740px;">
-            <div class="row g-0">
-
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h1 class="card-title">INI NANTI RUMAH RUBAH</h1>
-                        <p class="card-text">This is a wider card with supporting text below as a natural
-                            lead-in to
-                            additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-            </div>
-        </div> -->
 
         <!-- SAMPAI SINI-->
     </div>
