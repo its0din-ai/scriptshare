9c1360557e1def7d46600f090e74c4e57a7cd97d
SpyMonarch <muhammadaji1614@gmail.com>
2023-07-25 15:55:02 +0700

    head

--- app/views/includes/head.php
+++ app/views/includes/head.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,12 +13,11 @@
 
 
     <link rel="shortcut icon" href="/public/img/fav.png" type="image/x-icon">
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/public/css/app.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link rel="stylesheet" href="https://cdn.statically.io/gh/IanLunn/Hover/eb8629df/css/hover-min.css" />
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -33,9 +33,7 @@
         <div class="container-sm content-align-center">
             <a class="navbar-brand" href="/"><img src="/public/img/ss.png" width="50px" /></a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -47,7 +45,7 @@
                     <li class="nav-item me-auto">
                         <a class="nav-link" href="/#about">About</a>
                     </li>
-                    
+
                     <?php
                     $adaSesiLogin = isset($_SESSION['users']);
                     if ($adaSesiLogin) {
@@ -56,7 +54,7 @@
                             Dashboard
                         </button>
                         <ul class="dropdown-menu dropdown-menu-dark">
-                            <li><a class="dropdown-item" href="/dashboard">Home</a></li>
+                            <li><a class="dropdown-item" href="/dashboard">Profile</a></li>
                             
                             ';
                         if ($_SESSION['users']['roles'] === 'admin') {
@@ -76,7 +74,7 @@
                         echo '<li class="nav-item mt-md-auto mt-sm-2">
                                 <a href="/logout" class="btn btn-sm btn-outline-danger">Logout</a>
                             </li>';
-                    }else{
+                    } else {
                         echo '<li class="nav-item mt-md-auto mt-sm-2">
                                 <a href="/login" class="btn btn-sm btn-outline-light">Login</a>
                             </li>';
