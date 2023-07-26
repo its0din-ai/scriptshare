c63d7ea6079a13766b1e50d783d5ef0153f3fd6d
SpyMonarch <muhammadaji1614@gmail.com>
2023-07-26 14:39:40 +0700

    pp

--- app/views/index.php
+++ app/views/index.php
@@ -63,7 +63,7 @@
                     </div>
                     <div class="col-md-4 mb-3 mb-md-0">
                         <div class="card card-pfp">
-                            <img src="/public/img/j.jpg" class="card-img" alt="profil">
+                            <img src="/public/img/aji.jpg" class="card-img" alt="profil">
                             <div class="card-img-overlay align-items-end txt-pfp">
                                 <p class="card-text nama">Muhammad Aji Saputra</p>
                                 <p class="card-text nim">L200210030</p>
